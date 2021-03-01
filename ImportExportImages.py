9463ea4df9101a0be2a36ccad6c1abc257b3d708
albrook <alanbrook129@gmail.com>
2021-03-01 20:26:57 +1300

    Creating a class with methods related to importing and exporting images into the program. Also edited the gitignore to not upload images to the repository

---
+++ ImportExportImages.py
@@ -0,0 +1,61 @@
+import cv2 as cv
+from tkinter import filedialog
+
+class ImportExport():
+
+    def __init__(self):
+        pass
+
+    def importUnchangedImage(self, filename):
+        """
+
+        :return: None
+        """
+        image = None
+        try:
+            image = cv.imread(filename)
+        except(AttributeError, SystemError):
+            print("Failed to import selected file")
+        finally:
+            return image
+
+    def importColourImage(self, filename):
+        """
+
+        :return: None
+        """
+        image = None
+        try:
+            image = cv.imread(filename, cv.IMREAD_COLOR)
+        except(AttributeError, SystemError):
+            print("Failed to import selected file")
+        finally:
+            return image
+
+    def importGrayscaleImage(self, filename):
+        """
+
+        :return: None
+        """
+        image = None
+        try:
+            image = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+        except(AttributeError, SystemError):
+            print("Failed to import selected file")
+        finally:
+            return image
+
+    def exportImage(self, name, image):
+        try:
+            cv.imwrite(name, image)
+        except Exception:
+            print("There was an error saving the image")
+            return False
+        finally:
+            return True
+
+
+
+file = ImportExport()
+image = file.importGrayscaleImage("test.jpg")
+file.exportImage("testExport.jpg", image)
